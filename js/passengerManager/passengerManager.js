763d13bec0aa5816d8df7dec8aaff441461a28e2
Vandromme <Sebastien.Vandromme-ISC.France@fr.ibm.com>
2018-08-11 22:10:09 +0200

    clean passengerManager + fix numbers between 0 and max

--- js/passengerManager/passengerManager.js
+++ js/passengerManager/passengerManager.js
@@ -10,50 +10,56 @@ function passengerManager(game) {
 }
 
 
-passengerManager.prototype.create = function create() {
-
-	
-};
+passengerManager.prototype.create = function create() {};
 
 
 passengerManager.prototype.update = function update() {};
 
+//number can be positive or negative; there is no need for a remove function
 passengerManager.prototype.addEngineer = function addEngineer(number){
+  if(this.totalPassenger + number > this.maxPassenger) {
+    number = this.maxPassenger - this.totalPassenger;
+  }
   this.totalEngineer += number;
   this.totalPassenger += number;
+  if(this.totalEngineer < 0) {
+    this.totalEngineer = 0;
+  }
+  if(this.totalPassenger < 0){
+    this.totalPassenge = 0;
+  }
 };
 
 passengerManager.prototype.addSoldier = function addSoldier(number){
+  if(this.totalPassenger + number > this.maxPassenger) {
+    number = this.maxPassenger - this.totalPassenger;
+  }
   this.totalSoldier += number;
   this.totalPassenger += number;
+  if(this.totalSoldier < 0) {
+    this.totalSoldier = 0;
+  }
+  if(this.totalPassenger < 0){
+    this.totalPassenge = 0;
+  }
 };
 
 passengerManager.prototype.addCivilian = function addCivilian(number){
+  if(this.totalPassenger + number > this.maxPassenger) {
+    number = this.maxPassenger - this.totalPassenger;
+  }
   this.totalCivilian += number;
   this.totalPassenger += number;
-};
-
-passengerManager.prototype.removeEngineer = function removeEngineer(number){
-  this.totalEngineer -= number;
-  this.totalPassenger -= number;
-};
-
-passengerManager.prototype.removeSoldier = function removeSoldier(number){
-  this.totalSoldier -= number;
-  this.totalPassenger -= number;
-};
-
-passengerManager.prototype.removeCivilian = function removeCivilian(number){
-  this.totalCivilian -= number;
-  this.totalPassenger -= number;
+  if(this.totalCivilian < 0) {
+    this.totalCivilian = 0;
+  }
+  if(this.totalPassenger < 0){
+    this.totalPassenge = 0;
+  }
 };
 
 passengerManager.prototype.addMaxPassenger = function addMaxPassenger(number){
   this.maxPassenger += number;
-};
-
-passengerManager.prototype.removeMaxPassenger = function removeMaxPassenger(number){
-  this.maxPassenger -= number;
 };
 
 passengerManager.prototype.getEngineer = function getEngineer(){
@@ -62,22 +68,18 @@ passengerManager.prototype.getEngineer = function getEngineer(){
 };
 
 passengerManager.prototype.getSoldier = function getSoldier(){
-
   return this.totalSoldier;
 };
 
 passengerManager.prototype.getCivilian = function getCivilian(){
-
   return this.totalCivilian;
 };
 
 passengerManager.prototype.getTotalPassenger = function getTotalPassenger(){
-
   return this.totalPassenger;
 };
 
 passengerManager.prototype.getMaxPassenger = function getMaxPassenger(){
-
   return this.maxPassenger;
 };
 
