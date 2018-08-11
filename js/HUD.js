5fc072b9ffb89cdd13fb1ccb34f439c3f7e12d0f
boudesit <boudesit@gmail.com>
2018-08-11 16:07:25 +0200

    add eventManager

--- js/HUD.js
+++ js/HUD.js
@@ -1,122 +1,19 @@
 function HUD(game) {
 	this.game = game;
-	this.ressourcesManager = null;
-	this.player = null;
-	this.computer = null;
-	this.lvlManager = null;
-	this.beginEvent = true;
-	this.spriteBG = null;
-	this.fight = null;
-	this.fightCastle = null;
-	this.computeRessourcesTkt = null;
-	this.hero = null;
-	this.timeDelay = 0;
-	this.game.score = 0;
-	this.scoreText = '';
+
 	this.events = null;
 	this.currentEvent = null;
+	this.eventManager = null;
 };
 var style = { font: "bold 16px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
 HUD.prototype.create = function create() {
-
-	this.sprite = this.game.add.sprite(this.posX, this.posY, 'progressBar');
-	this.sprite.animations.add('idle');
-	this.sprite.animations.play('idle', 2, true);
-
-	this.timeDelay = 0;
-	this.events = dataEvents;
-	//  this.lvlManager = new lvlManager(this.game, 0);
-	//  this.lvlManager.create();
-	//
-	//  this.hero = new HeroManager(this.game,this.lvlManager);
-	//  this.hero.create();
-	//
-	//  this.explosionSound = game.add.audio('explosionSound');
-	//  this.explosion  = game.add.sprite(-100,-100, 'explosion');
-	//
-	//
-	// // this.spriteBG.animations.add('backgroundAnime');
-	// // this.spriteBG.animations.play('backgroundAnime', 10, true);
-	// //  The score
-	// this.scoreText = game.add.text(710, 10, this.game.score, { font: '30px Arial', fill: '#000000' });
-	// this.scoreText2 = game.add.text(610, 9, 'score :', { font: '30px Arial', fill: '#000000' });
-	//
-	// this.explosionSound = game.add.audio('explosionSound');
-	// this.explosion  = game.add.sprite(-100,-100, 'explosion');
+	this.eventManager = new eventManager(this.game);
+	this.eventManager.create();
 };
 
 
 
 HUD.prototype.update = function update() {
 
-	if (this.beginEvent) {
-		console.log('test');
-		cleanEvent(this.currentEvent);
-		this.currentEvent = {};
-		let event = this.events[getRandomInt(0, this.events.length)];
-
-		this.currentEvent.posX = 10;
-		this.currentEvent.posY = 40;
-
-		this.currentEvent.textDescription = game.add.text(this.currentEvent.posX, this.currentEvent.posY + 17, event.text, style);
-		this.currentEvent.choiceButtons = [];
-
-		event.choices.forEach((choice, index) => {
-			let button = game.add.button(this.currentEvent.posX + 5, this.currentEvent.posY + (35 * (1 + index)), 'button', actionOnClick, this, 2, 1, 0);
-			button.width = 500;
-			button.height = 30;
-			button.consequence = choice.consequence;
-			let textButton = game.add.text(this.currentEvent.posX + 50, this.currentEvent.posY + (35 * (1 + index) + 7), choice.text, style);
-			this.currentEvent.choiceButtons.push({ "button": button, "text": textButton });
-		});
-		this.beginEvent = false;
-	}
-	// this.hero.update();
-	// this.lvlManager.update();
-	// this.scoreText.setText(this.game.score);
-	//
-	// if (this.shakeWorld > 0)
-	// {
-	// 	var rand1 = game.rnd.integerInRange(-2,2);
-	// 	var rand2 = game.rnd.integerInRange(-2,2);
-	// 	game.world.setBounds(rand1, rand2, game.width + rand1, game.height + rand2);
-	// 	this.shakeWorld--;
-	// }
-	//
-	// if (this.shakeWorld == 0) {
-	// 	game.world.setBounds(0, 0, game.width,game.height);
-	// }
+	this.eventManager.update();
 };
-
-
-HUD.prototype.scoreScreen = function scoreScreen() {
-	this.music.pause();
-	this.game.state.start("GameScore");
-};
-
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function actionOnClick(button) {
-	console.log(button.consequence);
-	this.currentEvent.consequenceText = game.add.text(this.currentEvent.posX, this.currentEvent.posY + 200, button.consequence.text, style);
-	this.beginEvent = true;
-}
-
-function cleanEvent(event) {
-	if (event) {
-		console.log("destroy");
-		event.textDescription.destroy();
-		for (let button of event.choiceButtons) {
-			button.button.destroy();
-			button.text.destroy();
-		}
-		event.consequenceText.destroy();
-	} else {
-		console.log("not");
-	}
-}
--- js/eventManager/eventManager.js
+++ js/eventManager/eventManager.js
@@ -1,40 +1,73 @@
-var eventManager = function(game) {
-
+function eventManager(game) {
 	this.game = game;
-}
+  this.beginEvent = true;
+	this.events = null;
+	this.currentEvent = null;
+};
+var style = { font: "bold 16px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
 
-eventManager.prototype = {
-    create: function() {
+eventManager.prototype.create = function create() {
 
-    },
+	this.events = dataEvents;
+};
 
-    update: function() {
 
-      if (this.beginEvent) {
-    		this.currentEvent = {};
-    		let event = this.events[getRandomInt(0, this.events.length)];
 
-    		this.currentEvent.posX = 10;
-    		this.currentEvent.posY = 40;
+eventManager.prototype.update = function update() {
 
-    		this.currentEvent.textDescription = game.add.text(this.currentEvent.posX, this.currentEvent.posY + 17, event.text, style);
-    		this.currentEvent.choiceButtons = [];
+	if (this.beginEvent) {
+		console.log('test');
+		cleanEvent(this.currentEvent);
+		this.currentEvent = {};
+		let event = this.events[getRandomInt(0, this.events.length)];
 
-    		event.choices.forEach((choice, index) => {
-    			let button = game.add.button(this.currentEvent.posX + 5, this.currentEvent.posY + (35 * (1 + index)), 'button', actionOnClick, this, 2, 1, 0);
-    			button.width = 500;
-    			button.height = 30;
-    			button.consequence = choice.consequence;
-    			let textButton = game.add.text(this.currentEvent.posX + 50,this.currentEvent.posY + (35 * (1 + index) + 7), choice.text, style);
-    			this.currentEvent.choiceButtons.push({ "button": button, "text": textButton });
-    		});
-    		this.beginEvent = false;
-    	}
-    },
+		this.currentEvent.posX = 10;
+		this.currentEvent.posY = 40;
+
+		this.currentEvent.textDescription = this.game.add.text(this.currentEvent.posX, this.currentEvent.posY + 17, event.text, style);
+		this.currentEvent.choiceButtons = [];
+
+		event.choices.forEach((choice, index) => {
+			let button = this.game.add.button(this.currentEvent.posX + 5, this.currentEvent.posY + (35 * (1 + index)), 'button', actionOnClick, this, 2, 1, 0);
+			button.width = 500;
+			button.height = 30;
+			button.consequence = choice.consequence;
+			let textButton = this.game.add.text(this.currentEvent.posX + 50, this.currentEvent.posY + (35 * (1 + index) + 7), choice.text, style);
+			this.currentEvent.choiceButtons.push({ "button": button, "text": textButton });
+		});
+		this.beginEvent = false;
+	}
+};
+
+
+eventManager.prototype.scoreScreen = function scoreScreen() {
+	this.music.pause();
+	this.game.state.start("GameScore");
+};
+
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function actionOnClick(button) {
 	console.log(button.consequence);
-	this.currentEvent.consequenceText = game.add.text(50, 400, button.consequence.text, style);
+	this.currentEvent.consequenceText = this.game.add.text(this.currentEvent.posX, this.currentEvent.posY + 200, button.consequence.text, style);
 	this.beginEvent = true;
+}
+
+function cleanEvent(event) {
+	if (event) {
+		console.log("destroy");
+		event.textDescription.destroy();
+		for (let button of event.choiceButtons) {
+			button.button.destroy();
+			button.text.destroy();
+		}
+		event.consequenceText.destroy();
+	} else {
+		console.log("not");
+	}
 }
