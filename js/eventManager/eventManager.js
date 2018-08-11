542fb520278f49bbf8e9ee86bb6b214976881be3
Vandromme <Sebastien.Vandromme-ISC.France@fr.ibm.com>
2018-08-11 18:14:07 +0200

    add updateProgesssBars in eventManager

--- js/eventManager/eventManager.js
+++ js/eventManager/eventManager.js
@@ -75,6 +75,12 @@ async function actionOnClickChoice(button) {
 		console.log(button.consequence);
 		this.currentEvent.consequenceText = this.game.add.text(this.currentEvent.posX, this.currentEvent.nexElementPosY, button.consequence.text, style);
 		this.nextEventId = button.consequence.nextEvent;
+		updateProgessBars(
+			button.consequence.passenger ? Number(button.consequence.passenger) : null, 
+			button.consequence.energy ? Number(button.consequence.energy) : null, 
+			button.consequence.shield ? Number(button.consequence.shield) : null, 
+			button.consequence.search ? Number(button.consequence.search) : null
+		);
 		this.canClickButton = false;
 		await sleep(3000);
 		this.beginEvent = true;
