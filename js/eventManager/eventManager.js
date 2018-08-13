afffc0cc62cb4ae58ed0fb64091deef49ca08880
boudesit <boudesit@gmail.com>
2018-08-13 15:09:04 +0200

    change fix

--- js/eventManager/eventManager.js
+++ js/eventManager/eventManager.js
@@ -24,7 +24,7 @@ eventManager.prototype.create = function create() {
 	this.openWindowsSprite = this.game.add.sprite(12, 110, 'animDialogueOpen');
 
 	this.animDial = this.openWindowsSprite.animations.add('OpenDialog');
-	this.animDialFix = this.openWindowsSprite.animations.add('OpenDialogFix', [47], 10, true);
+	this.animDialFix = this.openWindowsSprite.animations.add('OpenDialogFix', [48], 10, true);
 
 
 	this.animDial.onComplete.add(function(){
@@ -40,7 +40,7 @@ eventManager.prototype.create = function create() {
 
 eventManager.prototype.update = function update() {
 	if(!this.beginEvent && !this.alreadyLaunch) {
-		this.openWindowsSprite.animations.play('OpenDialog',15, false);
+		this.openWindowsSprite.animations.play('OpenDialog',11, false);
 		this.alreadyLaunch = true;
 	} else if (this.beginEvent) {
 		this.cleanEvent(this.currentEvent);
